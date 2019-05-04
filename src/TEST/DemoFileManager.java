4300be2514e8e52973b3339690f228de9b430201
Serge SULA <sulabosiog@gmail.com>
2019-05-05 00:21:02 +0100

    RAS
    RAS

--- src/TEST/DemoFileManager.java
+++ src/TEST/DemoFileManager.java
@@ -23,7 +23,7 @@ import javax.swing.JFrame;
  *
  * @author user
  */
-public class Demo extends javax.swing.JFrame {
+public class DemoFileManager extends javax.swing.JFrame {
 
     /**
      * Creates new form Principal
@@ -31,7 +31,7 @@ public class Demo extends javax.swing.JFrame {
     public FileManager fm = new FileManager("http://www.visiterlardc.com/s2b/processeurS2B.php");
     private JFrame moi = null;
     
-    public Demo() {
+    public DemoFileManager() {
         initComponents();
         tabPrincipal.removeAll();
         moi = this;
@@ -862,21 +862,23 @@ public class Demo extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoFileManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoFileManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoFileManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DemoFileManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Demo().setVisible(true);
+                new DemoFileManager().setVisible(true);
             }
         });
     }

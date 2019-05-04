ff930fc0702bc6776828a06ac5ae66d3946f9c73
Serge SULA <sulabosiog@gmail.com>
2019-05-05 00:03:34 +0100

    RAS
    RAS

--- src/TEST/Demo.java
+++ src/TEST/Demo.java
@@ -23,7 +23,7 @@ import javax.swing.JFrame;
  *
  * @author user
  */
-public class Principal extends javax.swing.JFrame {
+public class Demo extends javax.swing.JFrame {
 
     /**
      * Creates new form Principal
@@ -31,7 +31,7 @@ public class Principal extends javax.swing.JFrame {
     public FileManager fm = new FileManager("http://www.visiterlardc.com/s2b/processeurS2B.php");
     private JFrame moi = null;
     
-    public Principal() {
+    public Demo() {
         initComponents();
         tabPrincipal.removeAll();
         moi = this;
@@ -862,20 +862,21 @@ public class Principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Demo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Principal().setVisible(true);
+                new Demo().setVisible(true);
             }
         });
     }

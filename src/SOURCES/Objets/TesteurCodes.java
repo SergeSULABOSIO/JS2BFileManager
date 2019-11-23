7a24f373141c23542004256e097680f3971ca11c
Serge SULA <sulabosiog@gmail.com>
2019-11-23 18:34:55 +0100

    Merge origin/master
    Conflicts:
    	build/built-jar.properties
    	build/classes/SOURCES/Objets/FileManager.class
    	dist/JS2BFileManager.jar
    	nbproject/private/private.properties
    	nbproject/private/private.xml

--- src/SOURCES/Objets/TesteurCodes.java
+++ src/SOURCES/Objets/TesteurCodes.java
@@ -5,13 +5,28 @@
  */
 package SOURCES.Objets;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author HP Pavilion
  */
 public class TesteurCodes {
+    public static final String data = "[LiaisonPeriodeFrais{idPeriode=1, nomPeriode=1er Trimestre, signaturePeriode=5450854614453354157, pourcentage=33.0}, LiaisonPeriodeFrais{idPeriode=2, nomPeriode=2ème Trimestre, signaturePeriode=-9105976197124345236, pourcentage=33.0}, LiaisonPeriodeFrais{idPeriode=3, nomPeriode=3ème Trimestre, signaturePeriode=-5421709498265554820, pourcentage=33.0}]";
+    
     public static void main(String[] a){
-        
+        Pattern pattern = Pattern.compile("LiaisonPeriodeFrais");
+        // in case you would like to ignore case sensitivity,
+        // you could use this statement:
+        // Pattern pattern = Pattern.compile("\\s+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(data);
+        // check all occurance
+        while (matcher.find()) {
+            //System.out.print("Start index: " + matcher.start());
+            //System.out.print(" End index: " + matcher.end() + " ");
+            System.out.println(matcher.group());
+        }
     }
     
 }
